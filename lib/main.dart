033f83fb72ca83d31ac778e78ce6c7dfc15265d0
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-01-08 13:09:18 +0530

    calling the routes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app_ui/constants.dart';
 import 'package:plant_app_ui/screens/home/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Plant App',
       theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
         primaryColor: kPrimaryColor,
         textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class Homescreen extends StatelessWidget {
+// TODO: Build navbar and body in components.
+
+import 'package:plant_app_ui/components/my_bottom_nav_bar.dart';
+import 'package:plant_app_ui/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+      bootomNavigationBar: MyBottomNavbar(),
+    );
   }
 }
