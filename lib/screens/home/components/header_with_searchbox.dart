93e7f722eb956080ba9e54b95d2873125547a669
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-01-09 21:17:54 +0530

    header with search bar

--- lib/screens/home/components/header_with_searchbox.dart
+++ lib/screens/home/components/header_with_searchbox.dart
@@ -7,6 +7,58 @@ class HeaderWithSearchBox extends StatelessWidget {
   const HeaderWithSearchBox({
     Key key,
     @required this.size,
-  })
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      //  It will cover 20% of our total height
+      height: size.height * 0.2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+              ),
+              height: size.height * 0.2 - 27,
+              decoration: BoxDecoration(
+                color: kPrimaryColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(36),
+                  bottomRight: Radius.circular(36),
+                ),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Hi Shambashib!',
+                    style: Theme.of(context).textTheme.headline5.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Image.asset("assets/images/logo.png")
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                height: 54,
+                )
+            )
+              ),
+        ])
+    )
+  }
 
 }
