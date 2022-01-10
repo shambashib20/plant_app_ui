4db01fdaa41e9ad4801ed6daa30d1251e4468675
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-01-10 19:06:04 +0530

    completed Headerwithsearch component

--- lib/screens/home/components/header_with_searchbox.dart
+++ lib/screens/home/components/header_with_searchbox.dart
@@ -54,11 +54,42 @@ class HeaderWithSearchBox extends StatelessWidget {
                 margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
                 padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
                 height: 54,
-                )
-            )
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: kPrimaryColor.withOpacity(0.23),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          hintText: "Search",
+                          hintStyle: TextStyle(
+                            color: kPrimaryColor.withOpacity(0.5),
+                          ),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          // surffix isn't working properly  with SVG
+                        // thats why we use row
+                        // suffixIcon: SvgPicture.asset("assets/icons/search.svg"),
+                        ),
+                      ),
+                    ),
+                    SvgPicture.asset("assets/icons/search.svg"),
+                  ],
+                ),
               ),
-        ])
-    )
+            ),
+          ],
+        ),
+    );
   }
-
 }
